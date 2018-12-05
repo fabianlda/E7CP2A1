3aa19caf83ed6e4abe776c77776f8d3b3baad505
fabian <fabian.makt@gmail.com>
2018-12-05 20:31:54 +0100

    actividad

--- ejr1.rb
+++ ejr1.rb
@@ -1,16 +1,16 @@
 puts '***Array aumentado en 1***'
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
 a = a.map { |e| e + 1 }
-puts  a
+puts a
 
 puts '***Array a float y string***'
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
-a = a.map { |a| a.to_f }
+a = a.map(&:to_f)
 puts a
 
 puts '***array a string***'
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
-a = a.map { |a| a.to_s }
+a = a.map(&:to_s)
 puts a
 
 puts '*** valor menores o igual a 5***'
@@ -30,7 +30,7 @@ puts a
 
 puts '***valores***'
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
-a = a.group_by { |x| x.even? }
+a = a.group_by(&:even?)
 puts a
 
 puts 'valores mayores y menores a 6'
--- ejr2.rb
+++ ejr2.rb
@@ -1,2 +1,8 @@
-nombres = ["Violeta", "Andino", "Clemente", "Javiera", "Paula", "Pia", "Ray"]
+nombres = %w[Violeta Andino Clemente Javiera Paula Pia Ray]
+nombres = nombres.select { |i| i.length > 5 }
+print nombres
+
+puts 'nombres en minusculas'
+nombres = %w[Violeta Andino Clemente Javiera Paula Pia Ray]
+nombres = nombres.map(&:downcase)
 print nombres
