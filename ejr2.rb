75dd908fa2e13e9f9e926a074e086ad1e3c174a0
fabian <fabian.makt@gmail.com>
2018-12-05 21:22:11 +0100

    actividad

--- ejr2.rb
+++ ejr2.rb
@@ -2,7 +2,22 @@ nombres = %w[Violeta Andino Clemente Javiera Paula Pia Ray]
 nombres = nombres.select { |i| i.length > 5 }
 print nombres
 
-puts 'nombres en minusculas'
+puts '***nombres en minusculas***'
 nombres = %w[Violeta Andino Clemente Javiera Paula Pia Ray]
 nombres = nombres.map(&:downcase)
+print nombres
+
+puts '***nombres con P***'
+nombres = %w[Violeta Andino Clemente Javiera Paula Pia Ray]
+nombres = nombres.select { |i| i.start_with? 'P' }
+print nombres
+
+puts 'cantidad de letras en el array'
+nombres = %w[Violeta Andino Clemente Javiera Paula Pia Ray]
+nombres = nombres.map(&:length)
+print nombres
+
+puts 'sin vocales'
+nombres = %w[Violeta Andino Clemente Javiera Paula Pia Ray]
+nombres = nombres.map { |x| x.gsub(/[aeiou]/, '') }
 print nombres
